1f11f7e827d542a84562c08e742b4ba11bd05156
Boed Winangun <boedwinangun@gmail.com>
2019-08-11 22:36:09 +0700

    add permission module

---
+++ racik/modules/permissions/config/config.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Racik
+ *
+ * An open source project to allow developers to Starter Web App of CodeIgniter-based
+ *
+ * @package   Racik
+ * @copyright Copyright (c) 2019
+ * @since     Version 1.2
+ * @link      https://github.com/boedwinangun/racik
+ * @filesource
+ */
+
+$config['module_config'] = array(
+	'description'	=> 'Manages the Permissions available to the Roles.',
+	'author'		=> 'Racik',
+    'name'          => 'lang:rp_menu_permissions',
+);
