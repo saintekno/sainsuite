1e848784c834ad52e467f029ff6bc9fabcfd0bbd
Boed Winangun <boedwinangun@gmail.com>
2019-08-11 17:53:55 +0700

    add activity module

---
+++ application/language/english/datatable_lang.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Racik
+ *
+ * Web App Starter CodeIgniter-based
+ *
+ * @package   Racik
+ * @copyright Copyright (c) 2019
+ * @since     Version 1.2
+ * @link      https://github.com/boedwinangun/racik
+ * @filesource
+ */
+
+$lang['sProcessing']	= 'Processing...';
+$lang['sLengthMenu']	= 'Show _MENU_ entries';
+$lang['sZeroRecords']	= 'No matching records found';
+$lang['sInfo']			= 'Showing _START_ to _END_ of _TOTAL_ entries';
+$lang['sInfoEmpty']		= 'Showing 0 to 0 of 0 entries';
+$lang['sInfoFiltered']	= '(filtered from _MAX_ total entries)';
+$lang['sInfoPostFix']	= '';
+$lang['sSearch']		= 'Search:';
+$lang['sUrl']			= '';
+$lang['sFirst']			= 'First';
+$lang['sPrevious']		= 'Previous';
+$lang['sNext']			= 'Next';
+$lang['sLast']			= 'Last';
