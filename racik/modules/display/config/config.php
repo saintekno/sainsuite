cb71700377b3fc8097323315354fb30fb4f14645
Boed Winangun <boedwinangun@gmail.com>
2019-08-11 22:29:42 +0700

    add display module

---
+++ racik/modules/display/config/config.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Racik
+ *
+ * An open source project to allow developers to Starter Web App of CodeIgniter-based
+ *
+ * @package   Racik
+ * @copyright Copyright (c) 2019
+ * @since     Version 1.2
+ * @link      https://github.com/boedwinangun/racik
+ * @filesource
+ */
+
+$config['module_config'] = array(
+    'author'      => 'Racik',
+    'description' => 'Provides helpers for consistent admin UI features.',
+    'name'        => 'lang:rp_menu_kb_shortcuts',
+    'version'     => '0.7.3',
+);
