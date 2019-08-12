ada06bc0ef0d70c970663255d8aa4074a6fa4ffd
Boed Winangun <boedwinangun@gmail.com>
2019-08-12 11:36:49 +0700

    add log modules

--- application/config/config.php
+++ application/config/config.php
@@ -234,7 +234,7 @@ $config['log_threshold'] = 1;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = APPPATH .'logs/';
 
 /*
 |--------------------------------------------------------------------------
---
+++ racik/modules/logs/config/config.php
@@ -0,0 +1,21 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Racik
+ *
+ * An open source project to allow developers to Starter Web App of CodeIgniter-based
+ *
+ * @package   Racik
+ * @copyright Copyright (c) 2019
+ * @since     Version 1.2
+ * @link      https://github.com/boedwinangun/racik
+ * @filesource
+ */
+
+$config['module_config'] = array(
+    'description' => 'Allows viewing of log files and adjusting log level.',
+    'author'      => 'Racik',
+    'name'        => 'lang:rp_menu_logs',
+    'version'     => '0.7.3',
+);
