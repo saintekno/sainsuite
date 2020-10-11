d18eb7242960c2cabd7f84435aafe33db3824dbd
Buddy Winangun <buddywinangun@gmail.com>
2020-10-12 01:44:37 +0700

    edit routes addons users

--- apps/addons/users/routes.php
+++ apps/addons/users/routes.php
@@ -3,5 +3,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 global $Routes;
 
-$Routes->get( 'users', 'UsersHomeController@index' );
-$Routes->get( 'users/profile', 'UsersHomeController@profile' );
+$Routes->get( 'users/{page_id?}', 'UsersHomeController@read' )
+->where([ 'page_id' => '[0-9]+' ]);
+
+$Routes->match([ 'get', 'post' ], 'users/create', 'UsersHomeController@create' );
+$Routes->match([ 'get', 'post' ], 'users/delete/{id}', 'UsersHomeController@delete' );
+$Routes->match([ 'get', 'post' ], 'users/edit/{id}', 'UsersHomeController@update' );
+
+$Routes->match([ 'get', 'post' ], 'users/profile', 'UsersProfileController@index' );
+
+$Routes->match([ 'get', 'post' ], 'users/groups', 'UsersHomeController@groups' );
