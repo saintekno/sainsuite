3c712ddfb72c0914fbcd62010060e934c09a30c2
Buddy Winangun <buddywinangun@gmail.com>
2020-10-12 02:38:46 +0700

    tambah event di addons users

--- apps/addons/users/events/actions.php
+++ apps/addons/users/events/actions.php
@@ -9,6 +9,58 @@ class Users_Action extends CI_model
         
         // $this->events->add_action( 'load_dashboard', [ $this, 'load_dashboard' ] );
         // $this->events->add_action( 'load_frontend', [ $this, 'load_frontend' ]  );
+        $this->events->add_action('load_users_custom_fields', array( $this, 'user_custom_fields' ));
+    }
+    
+    /**
+    * Adds custom fields for user creation and edit
+    *
+    * @access : public
+    * @param : Array
+    * @return : Array
+    **/
+    
+    public function user_custom_fields($config)
+    {
+        if ( $config[ 'mode' ] === 'edit' ) {
+            $this->polatan->add_item([
+                'type'      =>      'text',
+                'name'      =>      'first-name',
+                'label'     =>      __('First Name', 'aauth'),
+                'value'     =>      $this->aauth->get_user_var( 'first-name', $config[ 'user_id' ] ),
+            ], $config[ 'meta_namespace' ], $config[ 'col_id' ]);
+    
+            $this->polatan->add_item([
+                'type'      =>      'text',
+                'name'      =>      'last-name',
+                'label'     =>      __('Last Name', 'aauth'),
+                'value'     =>      $this->aauth->get_user_var( 'last-name', $config[ 'user_id' ] ),
+            ], $config[ 'meta_namespace' ], $config[ 'col_id' ]);  
+
+            $skin = $this->aauth->get_user_var( 'theme-skin', $config[ 'user_id' ]);
+        } 
+        else {
+            $this->polatan->add_item([
+                'type'      =>      'text',
+                'name'      =>      'first-name',
+                'label'     =>      __('First Name', 'aauth'),
+                'user_id'   =>      @$config[ 'user_id' ],
+            ], $config[ 'meta_namespace' ], $config[ 'col_id' ]);
+    
+            $this->polatan->add_item([
+                'type'      =>      'text',
+                'name'      =>      'last-name',
+                'label'     =>      __('Last Name', 'aauth'),
+                'user_id'   =>      @$config[ 'user_id' ],
+            ], $config[ 'meta_namespace' ], $config[ 'col_id' ]);  
+        }              
+        
+        riake( 'gui', $config )->add_item(array(
+            'type'        =>    'dom',
+            'content'    =>   $this->load->addon_view( 'users', 'custom-fields', compact( 'config' ), true )
+        ), $config[ 'meta_namespace' ], $config[ 'col_id' ]);
+
+        unset( $skin, $config );
     }
 }
 new Users_Action;
--- apps/addons/users/events/filters.php
+++ apps/addons/users/events/filters.php
@@ -10,6 +10,23 @@ class Users_Filters extends CI_model
         // $this->events->add_filter( 'signin_logo', [ $this->filters, 'signin_logo' ] );
         // $this->events->add_filter( 'dashboard_footer_right', [ $this->filters, 'dashboard_footer_right' ] );
         // $this->events->add_filter( 'dashboard_footer_text', [ $this->filters, 'dashboard_footer_text' ] );
+        $this->events->add_filter('custom_user_meta', array( $this, 'custom_user_meta' ), 10, 1);
+    }
+    
+    /**
+    * Adds custom meta for user
+    *
+    * @access : public
+    * @param : Array
+    * @return : Array
+    **/
+    
+    public function custom_user_meta($fields)
+    {
+        $fields[ 'first-name' ] = ($fname = $this->input->post('first-name')) ? $fname : '';
+        $fields[ 'last-name' ] = ($lname = $this->input->post('last-name')) ? $lname : '';
+        $fields[ 'theme-skin' ] = ($skin = $this->input->post('theme-skin')) ? $skin : 'skin-blue';
+        return $fields;
     }
 }
 new Users_Filters;
