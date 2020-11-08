f56c2094e8051d293dc6634581063f312ec63103
Buddy Winangun <buddywinangun@gmail.com>
2020-11-09 04:17:27 +0700

    fixed version

--- apps/config/about.php
+++ apps/config/about.php
@@ -38,7 +38,7 @@ $config[ 'supported_languages' ] = array(
 |--------------------------------------------------------------------------
 */
 $config[ 'app_name' ] = 'sainsuite';
-$config[ 'version' ] = '3.4.0';
+$config[ 'version' ] = '3.4.1';
 $config[ 'signature' ] = $config[ 'app_name' ] . ' ' . $config[ 'version' ];
 
 /*
