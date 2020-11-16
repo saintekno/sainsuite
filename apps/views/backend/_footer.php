e87089798cc41f8a5ea5e1af9aca5c46495616dc
Buddy Winangun <buddywinangun@gmail.com>
2020-11-17 03:04:15 +0700

    edit footer

--- apps/views/backend/_footer.php
+++ apps/views/backend/_footer.php
@@ -7,9 +7,9 @@
 
 		<!--begin::Copyright-->
 		<div class="text-muted order-2 order-md-1">
-			Powered by
+			<?php _e( 'Thank you for using' );?>
 			<a href="http://saintekno.id/" target="_blank" class="text-muted text-hover-primary font-weight-bold">
-				<?php echo $this->events->apply_filters('dashboard_footer_text', sprintf(__('%s'), 'SainTekno') );?>
+				<?php echo $this->events->apply_filters('dashboard_footer_text', sprintf( __( '%s' ), get( 'app_name' ) ) );?>
 			</a>
 		</div>
 
@@ -17,7 +17,7 @@
 		<div class="nav nav-dark order-1 order-md-2 text-muted"> 
 			<?php echo date('Y');?> &copy; &nbsp;
 			<a href="<?php echo site_url('admin/about'); ?>" class="nav-link p-0">
-			<?php echo $this->events->apply_filters( 'dashboard_footer_right', sprintf( __( '%s %s' ), get( 'app_name' ), get('version') ) );?>
+				<?php echo $this->events->apply_filters( 'dashboard_footer_right', sprintf( __( 'Version %s' ), get('version') ) );?>
 			</a>
 		</div>
 
