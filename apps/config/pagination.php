965e93438235d8c997425f9f93aa6554c0e08468
buddywinangun <buddywinangun@gmail.com>
2021-01-03 10:45:02 +0700

    fixed link pagination

--- apps/config/pagination.php
+++ apps/config/pagination.php
@@ -5,23 +5,11 @@ $config['full_tag_open']  = '<div class="d-flex flex-wrap p-2">';
 $config['full_tag_close'] = '</div>';
  
 $config['first_link'] = '<i class="ki ki-bold-double-arrow-back icon-xs"></i>';
-$config['first_tag_open'] = '<span class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">';
-$config['first_tag_close'] = '</span>';
-
 $config['prev_link'] = '<i class="ki ki-bold-arrow-back icon-xs"></i>';
-$config['prev_tag_open'] = '<span class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">';
-$config['prev_tag_close'] = '</span>';
- 
 $config['last_link'] = '<i class="ki ki-bold-double-arrow-next icon-xs"></i>';
-$config['last_tag_open'] = '<span class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">';
-$config['last_tag_close'] = '</span>';
- 
 $config['next_link'] = '<i class="ki ki-bold-arrow-next icon-xs"></i>';
-$config['next_tag_open'] = '<span class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">';
-$config['next_tag_close'] = '</span>';
 
 $config['cur_tag_open'] = '<span class="btn btn-icon btn-sm border-0 btn-hover-primary active mr-2 my-1">';
 $config['cur_tag_close'] = '</span>';
 
-$config['num_tag_open'] = '<span class="btn btn-icon btn-sm border-0 btn-light mr-2 my-1">';
-$config['num_tag_close'] = '</span>';
+$config['attributes'] = array('class' => 'btn btn-icon btn-sm border-0 btn-light-primary mr-2 my-1');
