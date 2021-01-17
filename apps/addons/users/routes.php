9217bbfa816d59200dfda3032842563ab50484a5
buddywinangun <buddywinangun@gmail.com>
2021-01-18 01:35:39 +0700

    edit url

--- apps/addons/users/routes.php
+++ apps/addons/users/routes.php
@@ -19,16 +19,16 @@ $Routes->group(['prefix' => '/users'], function () use ( $Routes ) {
     $Routes->get( '/{page_id?}', 'UsersHomeController@index' )->where([ 'page_id' => '[0-9]+' ]);
     $Routes->match([ 'get', 'post' ], 'add', 'UsersHomeController@add' );
     $Routes->match([ 'get', 'post' ], 'edit/{id}', 'UsersHomeController@edit' );
-    $Routes->match([ 'get', 'post' ], 'delete/{id}/{redirect?}', 'UsersHomeController@delete' );
-    $Routes->match([ 'get', 'post' ], 'multidelete', 'UsersHomeController@multidelete' );
+    $Routes->match([ 'get', 'post' ], 'delete/{id?}/{redirect?}', 'UsersHomeController@delete' );
 });
 
-$Routes->match([ 'get', 'post' ], 'profile', 'UsersProfileController@index' );
+$Routes->group(['prefix' => '/profile'], function () use ( $Routes ) {
+    $Routes->match([ 'get', 'post' ], '/{page?}', 'UsersProfileController@index' );
+});
 
 $Routes->group(['prefix' => '/group'], function () use ( $Routes ) {
     $Routes->match([ 'get', 'post' ], '/', 'GroupsHomeController@index' );
     $Routes->match([ 'get', 'post' ], 'add', 'GroupsHomeController@add' );
-    $Routes->match([ 'get', 'post' ], 'delete/{id}', 'GroupsHomeController@delete' );
     $Routes->match([ 'get', 'post' ], 'edit/{id}', 'GroupsHomeController@edit' );
-    $Routes->match([ 'get', 'post' ], 'multidelete', 'GroupsHomeController@multidelete' );
+    $Routes->match([ 'get', 'post' ], 'delete/{id?}', 'GroupsHomeController@delete' );
 });
