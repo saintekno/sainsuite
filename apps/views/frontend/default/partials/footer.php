80d082b56ca6b9c58a7a40b959d88c8b643a4eaa
buddywinangun <buddywinangun@gmail.com>
2021-02-04 07:06:50 +0700

    perbaikan theme

--- apps/views/frontend/default/partials/footer.php
+++ apps/views/frontend/default/partials/footer.php
@@ -5,5 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- javascript -->
 <script src="<?=base_url();?>assets/frontend/<?=theme_frontend();?>js/jquery-3.5.1.min.js"></script>
 <script src="<?=base_url();?>assets/frontend/<?=theme_frontend();?>js/bootstrap.bundle.min.js"></script>
+<script src="<?=base_url();?>assets/frontend/<?=theme_frontend();?>js/feather.min.js"></script>
+<script src="<?=base_url();?>assets/frontend/<?=theme_frontend();?>js/app.js"></script>
 </body>
 </html>
