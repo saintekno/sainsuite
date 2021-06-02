a87d0d7d479132a31675bbfe037b1483be7d1605
buddywinangun <buddywinangun@gmail.com>
2021-06-02 23:38:38 +0700

    fixed events

--- sainsuite/controllers/Welcome.php
+++ sainsuite/controllers/Welcome.php
@@ -27,13 +27,13 @@ class Welcome extends MY_Controller
 	public function index()
 	{
         $data['pages'] = 'home';
-        $this->load->frontend_view( 'layouts', $this->events->apply_filters('load_website_index', $data) );
+        $this->load->frontend_view( 'layouts', $this->events->apply_filters('fill_website_index', $data) );
 	}
 
     // not found page
     public function error_404()
     {
         $data['pages'] = '404';
-        $this->load->frontend_view( 'layouts', $this->events->apply_filters('load_website_index', $data) );
+        $this->load->frontend_view( 'layouts', $this->events->apply_filters('fill_website_index', $data) );
     }
 }
